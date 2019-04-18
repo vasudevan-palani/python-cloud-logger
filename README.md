8af9ff0c26d13f403d4d9fc08f9a8a6051440666
Palani <vasudevan.palani@gmail.com>
2019-04-18 15:14:13 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # python-cloud-logger
+
+A logger with thread local storage for logging context in all logs without repeating the code. This is a json logger where some contextual fields need to be included in every log per request/thread in a safe and efficient way.
+
+Note that, all the loggers retrieved on that particular thread ( by using logging.getLogger ) will inherit the context.
+
+## Usage
+```python
+
+from pythoncloudlogger import *
+
+logger=logging.getLogger("myapp-logger")
+
+logger.updateContext({
+  "requestId" : "1245"
+})
+
+logger.info("Request successfully processed")
+
+logger.clearContext()
+
+```
---
+++ pythoncloudlogger/index.py
@@ -0,0 +1,53 @@
+import logging
+import os, threading
+
+class ThreadDataManager():
+    data = {}
+    def __init__(self):
+        pass
+    @staticmethod
+    def getData():
+        ident = threading.currentThread().ident
+        if (ThreadDataManager.data.get(ident) == None):
+            ThreadDataManager.data[ident]={}
+        return ThreadDataManager.data.get(ident)
+
+    @staticmethod
+    def setData(data):
+        ident = threading.currentThread().ident
+        if (ThreadDataManager.data.get(ident) == None):
+            ThreadDataManager.data[ident]={}
+        ThreadDataManager.data[ident].update(data)
+        return ThreadDataManager.data.get(ident)
+
+    @staticmethod
+    def clearData(data):
+        ident = threading.currentThread().ident
+        if (ThreadDataManager.data.get(ident) == None):
+            ThreadDataManager.data[ident]={}
+        ThreadDataManager.data[ident].clear()
+
+_origFunc = logging.getLogger
+
+class _MyAdapter(logging.LoggerAdapter):
+    def __init__(self,logger,extra):
+        super().__init__(logger,extra)
+        self.logger = logger
+        self.handlers = logger.handlers
+    def hasHandlers(self,*args,**kargs):
+        return self.logger.hasHandlers(*args,**kargs)
+    def addHandler(self,*args,**kargs):
+        return self.logger.addHandler(*args,**kargs)
+    def removeHandler(self,*args,**kargs):
+        return self.logger.removeHandler(*args,**kargs)
+    def removeHandler(self,*args,**kargs):
+        return self.logger.removeHandler(*args,**kargs)
+    def updateContext(self,context):
+        ThreadDataManager.setData(context)
+    def clearContext(self,context):
+        ThreadDataManager.clearData()
+
+def newFunc(*args,**kargs):
+    logger =  _origFunc(*args,**kargs)
+    return _MyAdapter(logger,ThreadDataManager.getData())
+logging.getLogger = newFunc
