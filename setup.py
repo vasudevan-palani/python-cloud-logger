f24c54eccf7755fde5df9cdf7831f49ba1c887d1
vpalan799 <vasudevan_palani@comcast.com>
2021-01-14 06:35:03 -0500

    Bumped version and pushed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python-cloud-logger',
-    version='0.1.0',
+    version='0.1.1',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
